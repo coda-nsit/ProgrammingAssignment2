85c1743a1d3220cd3ce38e484b4b385a01765b2a
coda-nsit <crpbanerjee@gmail.com>
2016-02-11 20:00:56 +0530

    date 11 February, 2000 hrs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+#accept a matrix x
+#
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(passedMatrix){
+    x <<- passedMatrix
+    inv <- NULL
+  }
+  
+  get <- function(){
+    x
+  }
+  
+  setInv <- function(invVal){
+    inv <<-  invVal
+  }
+  
+  getInv <- function(){
+    inv
+  }
+  list(ste = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(is.null(inv) == FALSE){
+    message("getting cached value")
+    return(inv)
+  }
+  else{
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setInv(inv)
+    inv
+  }
+  ## Return a matrix that is the inverse of 'x'
 }
